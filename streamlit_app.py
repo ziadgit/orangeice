78ee1d937fe8796f88b6a4d7d44accf9601a6ca1
Ziad H <ziadbc@gmail.com>
2024-07-25 22:25:56 +0000

    rock

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
+import random
 
-st.title("🎈 My new app mod")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def get_computer_choice():
+    return random.choice(['Rock', 'Paper', 'Scissors'])
+
+def get_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return 'It\'s a tie!'
+    elif (user_choice == 'Rock' and computer_choice == 'Scissors') or \
+         (user_choice == 'Paper' and computer_choice == 'Rock') or \
+         (user_choice == 'Scissors' and computer_choice == 'Paper'):
+        return 'You win!'
+    else:
+        return 'Computer wins!'
+
+st.title('Rock-Paper-Scissors Game')
+
+user_choice = st.selectbox('Choose your option:', ['Rock', 'Paper', 'Scissors'])
+if st.button('Play'):
+    computer_choice = get_computer_choice()
+    result = get_winner(user_choice, computer_choice)
+
+    st.write(f'You chose: {user_choice}')
+    st.write(f'The computer chose: {computer_choice}')
+    st.write(f'Result: {result}')
